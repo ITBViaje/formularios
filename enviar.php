2205b539871cda9697d4017dd4beb5574cbb9412
buenviaje <106416282+ITBViaje@users.noreply.github.com>
2025-05-13 13:47:57 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'licmarialu@gmail.com'; // Tu cuenta Gmail
-        $mail->Password = 'drhe uhqy bwps sebs'; // Tu clave de aplicación
+        $mail->Password = 'drhe uhpy bwqs sebs'; // Tu clave de aplicación
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
